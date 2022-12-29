31b158664d3d8a39d00f6eda98b6bffa3d859b94
Andre Kristensen <andre.kristensen@lyse.net>
2022-12-29 15:43:48 +0100

    Added more qouetes

--- commands/fedon.js
+++ commands/fedon.js
@@ -5,6 +5,15 @@ const fedonQouetes = [
   "Du kan ikke få i både pose og skjegg",
   "Alle er ekspert når de dauer",
   "Jeg er hønen i revehuset",
+  "Det siste jeg trenger et 10-12kg mannebeist som prøver å spise meg mens jeg såver 😐",
+  "Når jeg er pensjonist så har jeg bare planer om å snakke om de gode gammle dagene og lage ekkle lyder med halsen mens jeg klør meg på ballene 😐",
+  "Det ansiktsutrykket der beskriver store deler av banrdommen min",
+  "Fingrene min har ikke ører, så de hørte deg tydeligvis ikke",
+  "Hvordan skal jeg få utvidet innsiktene min her uten at dere ytrer meningen deres!",
+  "Bare tanken på all idiotene der ute som enda ikke har byttet til vinterdekk er nok til at jeg tar hjemmekontor i dag",
+  "Jeg vet jeg er 6 dager eldre en @jgilje men jeg føler meg yngre 😐",
+  "Jeg har en ben å plukke med den påstanden!",
+  "Jeg skal faen google og mase her inne til alle feil er fikset!",
 ];
 module.exports = {
   data: new SlashCommandBuilder()
